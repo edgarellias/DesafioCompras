d0812cf3776926a27f597d0693f982eafa35183d
Edgar Elias <edgar_jk3@hotmail.com>
2023-05-22 20:48:12 -0300

    Desafio concluido

---
+++ src/Compra.java
@@ -0,0 +1,30 @@
+public class Compra implements Comparable<Compra> {
+    private String descricao;
+    private double valor;
+
+    public Compra(String descricao, double valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra: descricao = " + descricao + " valor ="+ valor;
+
+    }
+
+    @Override
+    public int compareTo(Compra outraCompra) {
+        return Double.valueOf(this.valor).compareTo(Double.valueOf(outraCompra.valor));
+    }
+}
